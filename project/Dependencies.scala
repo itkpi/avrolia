d0a0cb3efda0da84907b4fa8c768052981834768
0lejk4 <odubinskiy@ukr.net>
2019-01-12 16:32:06 +0200

    [Fix] extract variables

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,19 +2,21 @@ import sbt._
 
 object Dependencies {
   val `scala-2-12` = "2.12.8"
-  val testV = "3.0.4"
+  val testV        = "3.0.4"
+  val avroV        = "1.8.2"
+  val magnoliaV    = "0.10.0"
 
   object Testing {
-    val scalastic = "org.scalactic" %% "scalactic" % testV withSources()
-    val scalatest = "org.scalatest" %% "scalatest" % testV withSources()
+    val scalastic = "org.scalactic" %% "scalactic" % testV withSources ()
+    val scalatest = "org.scalatest" %% "scalatest" % testV withSources ()
   }
 
   object Avro {
-    val avro = "org.apache.avro" % "avro" % "1.8.2" withSources()
+    val avro = "org.apache.avro" % "avro" % avroV withSources ()
   }
 
   object Magnolia {
-    val magnolia = "com.propensive" %% "magnolia" % "0.10.0" withSources()
+    val magnolia = "com.propensive" %% "magnolia" % magnoliaV withSources ()
   }
 
 }
